c00b77fbfa10b6676be8e45278f4182c4f8c6d7a
Max Goodman <maximgoodman@gmail.com>
2019-01-27 15:07:53 -0500

    Working on graphing to JPanel

--- src/main/java/com/maximgoodman/GameOfLife/Grapher.java
+++ src/main/java/com/maximgoodman/GameOfLife/Grapher.java
@@ -1,15 +1,56 @@
 package com.maximgoodman.GameOfLife;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.List;
 
-public class Grapher {
+public class Grapher{
+
+    int maxX;
+    int maxY;
+    int xIndexes;
+    int xIncrement;
+    List livingList;
+
+
+
 
     public Grapher(List livingList){
 
-        JFrame graph = new JFrame();
-        graph.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        
+        this.livingList =livingList;
 
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+
+
+        JPanel graph = new JPanel();
+        Graphics2D graphics= (Graphics2D)g;
+        graph.paintComponent(graphics);
+
+        maxX = frame.getWidth();
+        maxY = frame.getHeight();
+        xIndexes = livingList.size()-1;
+        xIncrement = maxX/xIndexes;
+
+        //Graphics2D plotter = (Graphics2D)g;
+
+
+        //graph.repaint();
+        frame.add();
+        frame.setPreferredSize(new Dimension (400,600));
+        frame.pack();
+        frame.setVisible(true);
     }
+
+    @Override
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+        for(int i = 0; i <xIndexes;i++ ){
+            g.setColor(Color.red);
+            g.fillRect(i*xIncrement, (int)livingList.get(i)/100,5,5);
+            // graph.paintImmediately(i*xIncrement,(int)livingList.get(i)/100,20,20);
+        }
+    }
+
 }
